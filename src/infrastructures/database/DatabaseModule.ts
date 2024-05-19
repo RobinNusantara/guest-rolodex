fb9e927ff61b3c71fd13668331914985773cae88
RobinNusantara <robinnusantara127@gmail.com>
2024-05-19 22:41:27 +0700

    [ADD] mysql database infra

--- src/infrastructures/database/DatabaseModule.ts
+++ src/infrastructures/database/DatabaseModule.ts
@@ -2,6 +2,11 @@ import { Module } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { BookingModel } from "src/models/Booking/BookingModel";
+import { BookingToUnitModel } from "src/models/Booking/BookingToUnitModel";
+import { GuestModel } from "src/models/Guest/GuestModel";
+import { PayoutModel } from "src/models/Payout/PayoutModel";
+import { PropertyModel } from "src/models/Property/PropertyModel";
+import { UnitModel } from "src/models/Unit/UnitModel";
 
 @Module({
     imports: [
@@ -15,7 +20,14 @@ import { BookingModel } from "src/models/Booking/BookingModel";
                 username: configService.getOrThrow("MYSQL_USERNAME"),
                 password: configService.getOrThrow("MYSQL_PASSWORD"),
                 synchronize: configService.getOrThrow("MYSQL_SYNCHRONIZE"),
-                entities: [BookingModel],
+                entities: [
+                    BookingModel,
+                    BookingToUnitModel,
+                    GuestModel,
+                    PayoutModel,
+                    PropertyModel,
+                    UnitModel,
+                ],
             }),
         }),
     ],
