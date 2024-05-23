b78a1d8fb288ceac6b7c9be783038a7a882d2b5e
RobinNusantara <robinnusantara127@gmail.com>
2024-05-23 23:21:57 +0700

    [ADD] another tables

--- migrations/1716140142250-M20240520.ts
+++ migrations/1716140142250-M20240520.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable unused-imports/no-unused-vars */
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class M202405201716140142250 implements MigrationInterface {
@@ -11,7 +13,6 @@ export class M202405201716140142250 implements MigrationInterface {
                         type: "varchar",
                         length: "45",
                         isPrimary: true,
-                        isGenerated: true,
                     },
                     {
                         name: "guest_id",
@@ -51,6 +52,205 @@ export class M202405201716140142250 implements MigrationInterface {
                     },
                     {
                         name: "qbo_invoice_id",
+                        type: "varchar",
+                        length: "255",
+                        isNullable: true,
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default:
+                            "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
+                    },
+                ],
+            }),
+            true,
+        );
+
+        await queryRunner.createTable(
+            new Table({
+                name: "guests",
+                columns: [
+                    {
+                        name: "id",
+                        type: "varchar",
+                        length: "45",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                        length: "255",
+                        isNullable: false,
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                        length: "255",
+                        isNullable: false,
+                    },
+                    {
+                        name: "phone_number",
+                        type: "varchar",
+                        length: "255",
+                        isNullable: false,
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default:
+                            "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
+                    },
+                ],
+            }),
+            true,
+        );
+
+        await queryRunner.createTable(
+            new Table({
+                name: "booking_to_units",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "unit_id",
+                        type: "int",
+                        isNullable: false,
+                    },
+                    {
+                        name: "booking_id",
+                        type: "varchar",
+                        length: "45",
+                        isNullable: false,
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default:
+                            "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
+                    },
+                ],
+            }),
+            true,
+        );
+
+        await queryRunner.createTable(
+            new Table({
+                name: "units",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                        length: "255",
+                        isNullable: false,
+                    },
+                    {
+                        name: "property_id",
+                        type: "int",
+                        isNullable: false,
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default:
+                            "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
+                    },
+                ],
+            }),
+            true,
+        );
+
+        await queryRunner.createTable(
+            new Table({
+                name: "properties",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "booking_id",
+                        type: "varchar",
+                        length: "45",
+                        isNullable: false,
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default:
+                            "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
+                    },
+                ],
+            }),
+            true,
+        );
+
+        await queryRunner.createTable(
+            new Table({
+                name: "payouts",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                        length: "255",
+                        isNullable: false,
+                    },
+                    {
+                        name: "value",
+                        type: "decimal",
+                        precision: 11,
+                        scale: 2,
+                        isNullable: false,
+                    },
+                    {
+                        name: "qbo_expense_id",
                         type: "varchar",
                         length: "255",
                         isNullable: true,
