200da5d08386040bc532ec13b3f2c6bf25f7459b
RobinNusantara <robinnusantara127@gmail.com>
2024-05-24 05:06:55 +0700

    [ADD] insert booking

--- src/modules/bookings/controllers/BookingController.ts
+++ src/modules/bookings/controllers/BookingController.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { InsertBookingDto } from "src/dtos/Booking/InsertBookingDto";
 import { BookingService } from "src/modules/bookings/services/BookingService";
 
 @Controller("bookings")
 export class BookingController {
     constructor(private readonly bookingService: BookingService) {}
 
+    @Post()
+    public async insertBooking(@Body() body: InsertBookingDto): Promise<any> {
+        return await this.bookingService.insertBooking(body);
+    }
+
     @Get()
-    getBookings(): string {
-        return this.bookingService.getBookings();
+    public async getBookings(): Promise<any> {
+        return await this.bookingService.getBookings();
     }
 }
--- src/modules/bookings/services/BookingService.ts
+++ src/modules/bookings/services/BookingService.ts
@@ -1,8 +1,30 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { InsertBookingDto } from "src/dtos/Booking/InsertBookingDto";
+import { BookingModel } from "src/models/Booking/BookingModel";
+import { BookingToUnitModel } from "src/models/Booking/BookingToUnitModel";
+import { GuestModel } from "src/models/Guest/GuestModel";
+import { QuickbookService } from "src/modules/quickbook/services/QuicbookService";
+import { EntityManager, Repository } from "typeorm";
 
 @Injectable()
 export class BookingService {
-    getBookings(): string {
+    constructor(
+        private readonly entityManager: EntityManager,
+        @InjectRepository(BookingModel)
+        private readonly bookingRepository: Repository<BookingModel>,
+        @InjectRepository(GuestModel)
+        private readonly guestRepository: Repository<GuestModel>,
+        @InjectRepository(BookingToUnitModel)
+        private readonly bookingToUnitRepository: Repository<BookingToUnitModel>,
+        private readonly quickbookService: QuickbookService,
+    ) {}
+
+    public async insertBooking(body: InsertBookingDto): Promise<any> {
+        return body;
+    }
+
+    public async getBookings(): Promise<any> {
         return "Bookings";
     }
 }
